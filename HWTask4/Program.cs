b1b285bfaec04df7fcb7f23d915959594e939762
Aliya <karimova_aly@mail.ru>
2022-11-15 23:53:11 +0500

    Задача 4

---
+++ HWTask4/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+Console.WriteLine("Введите первое число");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberThird = Convert.ToInt32(Console.ReadLine());
+
+int m = numberFirst;
+if (numberSecond > m) m = numberSecond;
+else if (numberThird > m) m = numberThird;
+
+Console.WriteLine(m);
